b9e9cac4f611c2e07ef7edb2382ddc7a65d76fa5
Tim Wolff <timtwotoes@me.com>
2019-12-13 05:56:48 +0100

    Initial Commit

---
+++ URLSessionExtensions/URLSessionExtensions.h
@@ -0,0 +1,19 @@
+//
+//  URLSessionExtensions.h
+//  URLSessionExtensions
+//
+//  Created by Tim Wolff on 13/12/2019.
+//  Copyright © 2019 Tim Wolff. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for URLSessionExtensions.
+FOUNDATION_EXPORT double URLSessionExtensionsVersionNumber;
+
+//! Project version string for URLSessionExtensions.
+FOUNDATION_EXPORT const unsigned char URLSessionExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <URLSessionExtensions/PublicHeader.h>
+
+
